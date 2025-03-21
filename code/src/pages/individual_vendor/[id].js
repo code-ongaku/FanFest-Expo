4eedbf7c63babb3562d068362bcbf084b80cb32a
gabengu <gabengu@gmail.com>
2025-03-21 15:30:12 -0600

    Starter code for individual pages for event panel and vendor

---
+++ code/src/pages/individual_vendor/[id].js
@@ -0,0 +1,23 @@
+import { useRouter } from 'next/router';
+
+export async function getStaticPaths() {
+    const paths = [
+      { params: { id: '1' } }
+    ];
+  
+    return {
+      paths,
+      fallback: false
+    };
+}
+
+export default function Vendor() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  return (
+    <div className=''>
+        {id}
+    </div>
+  );
+}
