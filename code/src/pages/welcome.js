6a0ade4d61cee87b12e94afda3e25cdaad46c775
code-ongaku <108599236+code-ongaku@users.noreply.github.com>
2025-04-11 20:03:04 -0600

    Fixed link name

--- code/src/pages/welcome.js
+++ code/src/pages/welcome.js
@@ -51,7 +51,7 @@ const Welcome = () => {
           className="flex justify-center items-center gap-2 mt-1 cursor-pointer underline"
           onClick={() => router.push('/event_location')}
         >
-          <span>View venue map</span>
+          <span>View Venue on the Map</span>
           <div className="bg-[#D14D4D] w-6 h-6 rounded-full flex items-center justify-center shadow-md">
             <svg className="w-4 h-4 fill-white" viewBox="0 0 24 24">
               <path d="M12 2C8.14 2 5 5.14 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.86-3.14-7-7-7zm0 9.5a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5z" />
